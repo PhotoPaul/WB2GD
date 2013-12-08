f84c57c95c6b66461cdfa473f27a8ca3571bbf30
PhotoPaul <PhotoPaul@gmail.com>
2013-12-08 23:07:02 +0200

    Added google-api-php-client-0.6.7

--- src/index.php
+++ src/index.php
@@ -1,8 +1,13 @@
 <?php
 set_time_limit(0);
 
+loadGoogleDriveClient();
 
-
+function loadGoogleDriveClient(){
+    require_once "lib/google-api-php-client-0.6.7/src/Google_Client.php";
+    require_once "lib/google-api-php-client-0.6.7/src/contrib/Google_DriveService.php";
+    echo "loaded";
+}
 
 function mysqldump(){
     require("pdo.php");
